925be411f45ea5043d622820e91293e0eceb565f
julio_filizzola <juliofilizzola@hotmail.com>
2023-06-18 19:21:59 -0300

    feat: finally login

--- controllers/loginController.go
+++ controllers/loginController.go
@@ -1,7 +1,54 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/juliofilizzola/book_2/auth"
+	"github.com/juliofilizzola/book_2/initializers"
+	"github.com/juliofilizzola/book_2/models"
+	"net/http"
+)
 
 func Login(context *gin.Context) {
+	var body struct {
+		Email    string
+		Password string
+	}
 
+	err := context.Bind(&body)
+
+	if err != nil {
+		fmt.Println(err)
+		context.Status(http.StatusBadRequest)
+		return
+	}
+	var user models.User
+
+	result := initializers.DB.First(&user, "email = ?", body.Email)
+
+	if result.Error != nil {
+		fmt.Println(result.Error)
+		context.Status(http.StatusBadRequest)
+		return
+	}
+
+	err = auth.ValidPassword(user.Password, body.Password)
+
+	if err != nil {
+		fmt.Println(err)
+		context.Status(http.StatusBadRequest)
+		return
+	}
+
+	token, err := auth.GenerateToken(user.ID)
+
+	if err != nil {
+		fmt.Println(err)
+		context.Status(http.StatusBadRequest)
+		return
+	}
+
+	context.JSON(http.StatusAccepted, gin.H{
+		"token": token,
+	})
 }
